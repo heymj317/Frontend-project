1e513ef32efa982c64d19788cde8c8f17f231c5d
heymj317 <heymj317@users.noreply.github.com>
2022-07-08 13:03:09 -0500

    modified bootstrap styling

--- app.js
+++ app.js
@@ -117,11 +117,6 @@ function countdown() {
         let now = Date.now();
         let distance = timer - now;
 
-        // Find the distance between now and the count down date  
-        // Time calculations for days, hours, minutes and seconds
-        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Output the result in an element with id="demo"
